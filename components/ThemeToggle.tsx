0567f7c2ce0da17e415a50e84b29f47907a53a50
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-13 18:38:44 +0530

    Add components/ThemeToggle.tsx

---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// Theme toggle is disabled - always use dark theme with Heinicus Designs brand colors
+export function ThemeToggle() {
+  return <View />;
+}
